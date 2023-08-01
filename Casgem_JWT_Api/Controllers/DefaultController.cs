e93abb0ac485e327fc387aadcb1916088d6849ec
Yunus Emre Hardal <hardaalyunusemre@gmail.com>
2023-08-01 15:48:36 +0300

    Add project files.

---
+++ Casgem_JWT_Api/Controllers/DefaultController.cs
@@ -0,0 +1,25 @@
+﻿using Casgem_JWT_Api.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Casgem_JWT_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet("[Action]")]
+        public IActionResult Login()
+        {
+            return Created("", new BuildToken().CreateToken());
+        }
+
+        [Authorize]
+        [HttpGet("[Action]")]
+        public IActionResult CustomerList()
+        {
+            return Ok("Müşteri listesine eriştiniz.");
+        }
+    }
+}
